bfe621767893cda9845bce803896badde7301a60
Alonso Donayre <alonso2295@hotmail.com>
2022-10-16 15:16:05 -0500

    First Commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int myFirstNumber = 10;
+        int mySecondNumber = 2;
+        int myThirdNumber = 3;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+    }
+}
+
+
---
+++ PrimitiveTypes/src/academy/learning/Main.java
@@ -0,0 +1,32 @@
+package academy.learning;
+
+public class Main {
+    public static void main(String [] args) {
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Min Value: " + myMinIntValue);
+        System.out.println("Max Value: " + myMaxIntValue);
+        
+        int myMaxIntTest = 2_147_483_647;
+        
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Min Value: " + myMinByteValue);
+        System.out.println("Byte Max Value: " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Min Value: " + myMinShortValue);
+        System.out.println("Short Max Value: " + myMaxShortValue);
+
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+        System.out.println("Long Min Value: " + myMinlongValue);
+        System.out.println("Long Max Value: " + myMaxlongValue);
+        
+        
+    }
+}
